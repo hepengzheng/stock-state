bf0a9b825f4971ba466597278bb9e2804bf9dec5
hepengzheng <hepengzheng@hotmail.com>
2025-01-02 14:14:41 +0800

    close the session for a leadership

--- pkg/election/leadership.go
+++ pkg/election/leadership.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"sync/atomic"
 
-	"github.com/hepengzheng/stock-state/pkg/logutil"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/client/v3/concurrency"
+
+	"github.com/hepengzheng/stock-state/pkg/logutil"
 )
 
 const defaultSessionTTL = 5 // seconds
@@ -21,6 +22,7 @@ type Leadership struct {
 	leaderKey string
 	leaderID  string // we may use the container IP as leaderID
 
+	session  *concurrency.Session
 	election *concurrency.Election
 
 	// currentLeaderID stores the id of the current leader
@@ -42,7 +44,7 @@ func (ls *Leadership) Start(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-
+	ls.session = sess
 	ls.election = concurrency.NewElection(sess, ls.leaderKey)
 	for {
 		select {
@@ -63,8 +65,13 @@ func (ls *Leadership) Start(ctx context.Context) error {
 }
 
 func (ls *Leadership) Stop(ctx context.Context) {
-	ls.currentLeaderID.Store("")
+	ls.currentLeaderID = atomic.Value{}
 	_ = ls.election.Resign(ctx)
+	if ls.session != nil {
+		err := ls.session.Close()
+		if err != nil { // TODO(log)
+		}
+	}
 }
 
 func (ls *Leadership) IsLeader() bool {
