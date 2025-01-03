606531999bf4bf93817ca0058c63b331b24df67e
hepengzheng <hepengzheng@hotmail.com>
2025-01-03 17:09:51 +0800

    add zap logger

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/hepengzheng/stock-state
 
 go 1.23.4
 
-require go.etcd.io/etcd/client/v3 v3.5.17
+require (
+	go.etcd.io/etcd/client/v3 v3.5.17
+	go.uber.org/zap v1.27.0
+)
 
 require (
 	github.com/coreos/go-semver v0.3.0 // indirect
@@ -11,9 +14,7 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.17 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.17 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.17.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ pkg/logger/zaplogger.go
@@ -0,0 +1,61 @@
+package logger
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var m sync.Mutex
+var globalLogger atomic.Value // use Logger for type-safe and performance
+
+func init() {
+	cfg := zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:         "json",
+		OutputPaths:      []string{"stdout", "server.log"},
+		ErrorOutputPaths: []string{"stderr", "server.err.log."},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey: "msg",
+		},
+	}
+	logger, err := cfg.Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m.Lock()
+	defer m.Unlock()
+	globalLogger.Store(logger)
+}
+
+func ll() *zap.Logger {
+	return globalLogger.Load().(*zap.Logger)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	ll().Info(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	ll().Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	ll().Error(msg, fields...)
+}
+
+func Debug(msg string, fields ...zap.Field) {
+	ll().Debug(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	ll().Fatal(msg, fields...)
+}
+
+func Sync() error {
+	return ll().Sync()
+}
